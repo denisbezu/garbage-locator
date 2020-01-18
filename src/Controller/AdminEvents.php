71c7d282b4262867c5cd4d737ca849d40f8daf2e
Bogdan Yampolskiy <byampolskiy@202-ecommerce.com>
2020-01-18 16:22:52 +0200

    refs #23 deleting of the images

--- src/Controller/AdminEvents.php
+++ src/Controller/AdminEvents.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Event;
-use App\Entity\Marker;
+use App\Entity\Image;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminEvents extends AbstractController
@@ -30,7 +31,7 @@ class AdminEvents extends AbstractController
 
     /**
      *
-     * @Route("/admin/event/{id}", name="admin_event")
+     * @Route("/admin/event/{id}", name="admin_event", requirements={"id": "\d+"})
      */
     public function showEvent(int $id): Response
     {
@@ -52,5 +53,32 @@ class AdminEvents extends AbstractController
             ->find($id);
 
         return $this->render('admin/event.html.twig', ['event' => $event]);
+    }
+
+    /**
+     *
+     * @Route("/admin/event/deleteImage", name="admin_event_delete_image")
+     */
+    public function deleteImage(Request $request): JsonResponse
+    {
+        try {
+            $idImage = (int)$request->get('idImage');
+            $manager = $this->getDoctrine()->getManager();
+            $image = $manager->getRepository(Image::class)->find($idImage);
+            $manager->remove($image);
+            $manager->flush();
+
+            $response = array(
+                'success' => true,
+                'errorMessage' => ''
+            );
+        } catch (\Exception $e) {
+            $response = array(
+                'success' => false,
+                'errorMessage' => $e->getMessage()
+            );
+        }
+
+        return new JsonResponse($response, Response::HTTP_OK, []);
     }
 }
